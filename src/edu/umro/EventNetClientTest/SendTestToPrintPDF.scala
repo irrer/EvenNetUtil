6bc6dd7c5423b74b1d31d8b130c53e9546a52a19
Jim Irrer <irrer@umich.edu>
2019-09-11 18:57:29 -0400

    update library ref

--- src/edu/umro/EventNetClientTest/SendTestToPrintPDF.scala
+++ src/edu/umro/EventNetClientTest/SendTestToPrintPDF.scala
@@ -14,6 +14,8 @@ object SendTestToPrintPDF {
 
   def createConnection(host: String, port: Int) = {
     val factory = new com.rabbitmq.client.ConnectionFactory
+    factory.setUsername("eventnet")
+    factory.setPassword("waygranted")
     val cp = factory.getClientProperties();
     val props = cp.keySet.toArray.map(k => (k.toString + "=" + cp.get(k)))
     println("Client Properties: " + props.mkString("\n    ", "\n    ", "\n    "))
@@ -30,10 +32,15 @@ object SendTestToPrintPDF {
     //def sendEvent(channel: Channel) = channel.basicPublish(exchange, config.RoutingKeyPrefix + eventName, null, content.getBytes())
 
     val channel = connection.createChannel
+    println("exchange: " + exchange + "    routingKey: " + routingKey)
+
     channel.basicPublish(exchange, routingKey, null, content.getBytes)
 
-    printf("Sent content:\n" + content)
+    println("Sent content:\n" + content)
   }
+
+  // <Destination>C:\Program Files\UMRO\PrintPDF2-2.0.2\tmp\test</Destination>
+  // <Destination>E:\Program Files\UMRO\PrintPDF\PrintPDF2-2.0.2\tmp\foo.pdf</Destination>
 
   private val printPdfTestEvent =
     """<Event type="UMRO.UMPLAN.PrintPDF">
@@ -44,7 +51,7 @@ object SendTestToPrintPDF {
     <Plan>0</Plan>
     <Host>REMUS</Host>
     <Directory>$1$DGA1103:[IRRER]</Directory>
-    <Destination>E:\Program Files\UMRO\PrintPDF\PrintPDF2-2.0.2\tmp\foo.pdf</Destination>
+    <Destination>E:\Program Files\UMRO\PrintPDF\PrintPDF2-2.0.2\tmp\test\foo.pdf</Destination>
   </MetaData>
   <FileList>
     <File>TEST3D_01_DVH.TXT</File>
@@ -72,26 +79,34 @@ object SendTestToPrintPDF {
 """
 
   def main(args: Array[String]): Unit = {
-    println("Starting ...")
+    try {
+      println("Starting ...")
 
-    // "141.214.124.176" // "rodicom11cet" // "rodicom11prod" // "rodicom11dev" //  "localhost" // "10.30.3.90" // "uhroappwebsdv1" // "rodicom11cet" // "172.20.125.28"
+      // "141.214.124.176" // "rodicom11cet" // "rodicom11prod" // "rodicom11dev" //  "localhost" // "10.30.3.90" // "uhroappwebsdv1" // "rodicom11cet" // "172.20.125.28"
 
-    val host = {
-      val env = System.getenv("AMQPBrokerHost")
-      if (env != null) env
-      else "localhost"
+      val host = {
+        val env = System.getenv("AMQPBrokerHost")
+        if (env != null) env
+        else "localhost"
+        "10.30.3.90" // "uhroappwebsdv1" //   "rodicom11cet" //  "rodicom11cet" //
+      }
+      val port = 5672
+      val exchange = ""
+      val routingKey = "UMRO.UMPlan.PrintPDF"
+
+      val connection = createConnection(host, port)
+
+      sendEvent(connection, exchange, routingKey, printPdfTestEvent)
+
+      println("Sleeping ...")
+      Thread.sleep(3 * 1000L)
+      println("Done.  Exiting.")
+    } catch {
+      case t: Throwable => {
+        println("Unexpected exception in SendTestToPrintPDF")
+        t.printStackTrace
+      }
     }
-    val port = 5672
-    val exchange = ""
-    val routingKey = "UMRO.UMPlan.PrintPDF"
-
-    val connection = createConnection(host, port)
-
-    sendEvent(connection, exchange, routingKey, printPdfTestEvent)
-
-    println("Sleeping ...")
-    Thread.sleep(3 * 1000L)
-    println("Done.  Exiting.")
     System.exit(0)
   }
 
